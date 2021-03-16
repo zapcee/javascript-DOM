1b15142fa5929806953b82867d06f9d8a0789b9f
Kristians Artis Berzins <kristiansberzins@outlook.lv>
2021-03-16 15:32:07 +0200

    5th lesson finished

--- 05-toggle-elements-of-dom-path/QUESTIONS.md
+++ 05-toggle-elements-of-dom-path/QUESTIONS.md
@@ -6,4 +6,4 @@
 
 > Describe the contract you used for finding the movies to toggle in the DOM. How is this function different from other functions that find elements in the DOM?
 
-Your reply here...
+I used the function `querySelectorAll` - it returns all element descendants of node that match selectors, which in my case were: elements `li` in the class `.second_five`.
--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,14 @@
-// Your JavaScript goes here.
+window.addEventListener('load', function(){
+    const contentToToggle = document.querySelectorAll('.second_five li');
+    const buttonToggle = document.getElementById("toggle_button");
+
+    buttonToggle.addEventListener('click', function(){
+        contentToToggle.forEach((v) => {
+            if(v.style.display == 'none') {
+                v.style.display = 'list-item'
+            } else {
+                v.style.display = 'none'
+            }
+        });
+    });
+});
