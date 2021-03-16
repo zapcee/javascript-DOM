a405e235bdf65010761c76fb4d9d0c33c5f5d348
Kristians Artis Berzins <kristiansberzins@outlook.lv>
2021-03-16 15:19:31 +0200

    4th lesson finished

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,14 @@
-// Your JavaScript goes here.
+window.addEventListener('load', function(){
+    const toggleContent = document.getElementsByClassName("toggle_me");
+    const toggleClick = document.getElementById("button");
+
+    toggleClick.addEventListener('click', function(){
+        Array.from(toggleContent).forEach((v) => {
+            if(v.style.display == 'none') {
+                v.style.display = 'flex'
+            } else {
+                v.style.display = 'none'
+            }
+        });
+    });
+});
