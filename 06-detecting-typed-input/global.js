182a8383ea5c07701e025f000fb58a3e459f45aa
Kristians Artis Berzins <kristiansberzins@outlook.lv>
2021-03-16 15:44:08 +0200

    6th lesson finished

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,11 @@
-// Your JavaScript goes here.
+window.addEventListener('load', function(){
+    
+    const field = document.getElementById("full_name");
+    const destinationParagraph = document.getElementById('greeting');
+    
+    field.addEventListener('keyup', (e) => {
+        let text = e.target.value;
+        destinationParagraph.innerHTML = ("Hello there, " + text + "!");
+    });
+
+});
