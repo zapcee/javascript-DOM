9867d04762aaf756088fffd6a0a0b6da0763733f
Kristians Artis Berzins <kristiansberzins@outlook.lv>
2021-03-16 16:25:08 +0200

    7th lesson finished

--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,24 @@
-// Your JavaScript goes here.
+window.addEventListener('load', function(){
+    
+    const first = document.getElementById('first');
+    const second = document.getElementById('second');
+    const third = document.getElementById('third');
+
+    first.addEventListener('keyup', () => {
+        inputText = first.value.length;
+        maxLength = parseInt(first.getAttribute("maxlength"));
+
+        if (inputText == maxLength){
+            second.focus();
+        }
+    });
+
+    second.addEventListener('keyup', () => {
+        inputText = second.value.length;
+        maxLength = parseInt(second.getAttribute("maxlength"));
+
+        if (inputText == maxLength){
+            third.focus();
+        }
+    });
+});
